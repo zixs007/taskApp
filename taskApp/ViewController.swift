78008e5cf100d6e8153aadcf9474eab8d68b0743
yuto <yuto00711127@ezweb.ne.jp>
2021-01-07 22:46:30 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 河崎優人 on 2021/01/07.
+//  Copyright © 2021 yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
